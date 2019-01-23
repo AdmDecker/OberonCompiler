ffa8e71e818762623c658970876a8b57cd7059c1
AdmDecker <AdmDecker@gmail.com>
2019-01-23 08:28:11 -0600

    initial commit

---
+++ OberonCompiler/Analyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OberonCompiler
+{
+    enum tokens { numt, stringt, }
+
+    class Analyzer
+    {
+        List<string> ReservedWords = new List<string> {
+            "MODULE", "PROCEDURE", "VAR", "BEGIN",
+            "END", "IF", "THEN", "ELSE", "ELSIF",
+            "WHILE", "DO", "ARRAY", "RECORD", "CONST",
+            "TYPE" };
+
+    }
+}
